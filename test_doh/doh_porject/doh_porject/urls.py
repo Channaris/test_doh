cc0241d4cf57deee03382832ff3034f6cb2f175f
WIEW <c.b.w.channaris@gmail.com>
2023-01-09 16:53:16 +0700

    fail

--- test_doh/doh_porject/doh_porject/urls.py
+++ test_doh/doh_porject/doh_porject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
+    # path("__reload__/", include("django_browser_reload.urls")),
 ]
